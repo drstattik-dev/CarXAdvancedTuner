1bad38a3f95fb64e91011f704cc3d046f4da2e98
DrSTaTTiK <54222325+drstattik-dev@users.noreply.github.com>
2023-03-01 19:23:11 -0500

    Delete obj directory

--- obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CarXTuner
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "CarXTuner";
-        public const string PLUGIN_NAME = "CarXTuner";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
