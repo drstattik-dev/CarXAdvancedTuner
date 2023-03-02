9cb52e6f6428f1ed8683ab7494b596f5087ae76e
DrSTaTTiK <drstattik2242@gmail.com>
2023-03-01 19:56:34 -0500

    Revert "Delete obj directory"
    This reverts commit 1bad38a3f95fb64e91011f704cc3d046f4da2e98.

---
+++ obj/Debug/netstandard2.0/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace CarXTuner
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "CarXTuner";
+        public const string PLUGIN_NAME = "CarXTuner";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
